51979285e37a7a517d4a75e170bfda0792e6a3f3
Berna Köse <anreb_55@hotmail.com>
2021-12-17 15:54:32 +0300

    Add project files.

---
+++ WebScraping/Controllers/EmploymentController.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebScraping.Models;
+
+namespace WebScraping.Controllers
+{
+    public class EmploymentController : Controller
+    {
+        // GET: Employment
+        public ActionResult Index()
+        {
+            List<Home> homes = new List<Home>();
+            var web = new HtmlWeb();
+            var doc = web.Load("https://www.hepsiemlak.com/satilik");
+
+            foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='upper sibling']"))
+            {
+                string title = item.ChildNodes[0].ChildNodes[0].InnerText.Trim();
+                homes.Add(new Home()
+                {
+                    title = title
+                });
+            }
+            
+
+            return View(homes);
+        }
+    }
+}
---
+++ WebScraping/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebScraping.Models
+{
+    public class Home
+    {
+        public string title { get; set; }
+        public string link { get; set; }
+
+    }
+}
