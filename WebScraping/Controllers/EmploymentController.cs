f2c02b26a6d3764bec7fdcbc2bce05bd6e7c9614
Berna Köse <anreb_55@hotmail.com>
2022-01-28 22:44:54 +0300

    28.01.2022

--- WebScraping/Controllers/EmploymentController.cs
+++ WebScraping/Controllers/EmploymentController.cs
@@ -42,26 +42,33 @@ namespace WebScraping.Controllers
 
 
             List<Home> homes = new List<Home>();
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < 25; i++)
             {
                 var web = new HtmlWeb();
                 var doc = web.Load("https://www.hepsiemlak.com/kiralik?page=" + i);
                 foreach (var item in doc.DocumentNode.SelectNodes("//div[@class='list-view-line']"))
                 {
-                    string title = item.ChildNodes[2].ChildNodes[2].ChildNodes[0].ChildNodes[0].InnerText.Trim();
+                    string price = item.ChildNodes[2].ChildNodes[2].ChildNodes[0].ChildNodes[0].InnerText.Trim();
                     string city = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[2].ChildNodes[0].ChildNodes[2].ChildNodes[2].InnerText.Trim();
-                    string room = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[2].ChildNodes[0].ChildNodes[0].InnerText.Trim();
+                    string age = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[2].ChildNodes[6].ChildNodes[0].InnerText.Trim();
+                    string floor = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[2].ChildNodes[8].ChildNodes[0].InnerText.Trim();
+                    string room = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[2].ChildNodes[0].InnerText.Trim();
+                    string metre = item.ChildNodes[2].ChildNodes[4].ChildNodes[0].ChildNodes[0].ChildNodes[0].ChildNodes[2].ChildNodes[4].ChildNodes[0].InnerText.Trim();
                     string link = "https://www.hepsiemlak.com" + item.ChildNodes[2].ChildNodes[0].ChildNodes[0].GetAttributeValue("href", "").Trim();
-                    string img = item.ChildNodes[0].ChildNodes[2].ChildNodes[2].GetAttributeValue("src", "").Trim();
+                    string img = item.ChildNodes[0].ChildNodes[2].ChildNodes[2].GetAttributeValue("data-src", "").Trim();
                     homes.Add(new Home()
                     {
-                        title = title,
+                        price = price,
                         city = city,
                         room = room,
+                        age = age,
+                        floor=floor,
+                        metre = metre,
                         link = link,
                         image = img
-                    });
+                    }); ;
                 }
+
             }
             return View(homes);
         }
--- WebScraping/Models/Home.cs
+++ WebScraping/Models/Home.cs
@@ -7,8 +7,10 @@ namespace WebScraping.Models
 {
     public class Home
     {
-        public string title { get; set; }
+        public string price { get; set; }
         public string city { get; set; }
+        public string age { get; set; }
+        public string floor { get; set; }
         public string room { get; set; }
         public string metre { get; set; }
         public string link { get; set; }
